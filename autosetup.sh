a7eea8ee8003f8922974776671a44de59c0bd554
techflashYT <72118300+techflashYT@users.noreply.github.com>
2023-12-24 21:50:49 -0700

    detect if we are in the arch linux installer

--- autosetup.sh
+++ autosetup.sh
@@ -21,32 +21,67 @@ dots "\e[1;33m3" "."
 dots "\e[0;33m2" "."
 dots "\e[31m1" "!"
 
-echo -e "\n\e[0mInstalling..."
-if [ "$(id -u)" != "0" ]; then
-	echo -e "\e[31mERROR: You must be root to run this script"
-	exit 1
+
+hostname="$(cat /etc/hostname)"
+awk '/iwctl/ && /nmcli/ && /utility/ && /Wi-Fi, authenticate to the wireless network using the/' /etc/motd
+awkRet=$?
+
+isArchISO=false
+if [ "$hostname" = "archiso" ] && [ "$awkRet" = "0" ]; then
+	isArchISO=true
 fi
-echo "Installing packages..."
-pacman -S --noconfirm --needed sudo git base-devel rsync \
-pipewire pipewire-pulse pavucontrol 
 
 
-echo "Adding user and sudo setup"
-groupadd -r sudo
-sed -i 's/# %sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
-useradd -m techflash -c Techflash -G users,sudo,plugdev,video,render
-echo "Please enter the password for the new user"
-passwd techflash
-
-echo "Running dotfiles setup"
+###########################################
+#                                         #
+#   I N - I N S T A L L E R   S E T U P   #
+#                                         #
+###########################################
 
 
-echo "Adding autologin to getty config"
-mkdir /etc/systemd/system/getty@tty1.service.d
-cat << EOF > /etc/systemd/system/getty@tty1.service.d/autologin.config
+
+
+###########################################
+#                                         #
+#   I N S T A L L E D   O S   S E T U P   #
+#                                         #
+###########################################
+mainSetup() {
+	echo -e "\n\e[0mInstalling..."
+	if [ "$(id -u)" != "0" ]; then
+		echo -e "\e[31mERROR: You must be root to run this script"
+		exit 1
+	fi
+	echo "Installing packages..."
+	pacman -S --noconfirm --needed sudo git base-devel rsync \
+	pipewire pipewire-pulse pavucontrol 
+
+
+	echo "Adding user and sudo setup"
+	groupadd -r sudo
+	sed -i 's/# %sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
+	useradd -m techflash -c Techflash -G users,sudo,plugdev,video,render
+	echo "Please enter the password for the new user"
+	passwd techflash
+
+	echo "Running dotfiles setup"
+
+	echo "Adding autologin to getty config"
+	mkdir /etc/systemd/system/getty@tty1.service.d
+	cat << EOF > /etc/systemd/system/getty@tty1.service.d/autologin.config
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty -o '-p -f -- \\\\u' --noclear --autologin techflash %I \$TERM
 EOF
+}
 
 
+
+
+if [ "$isArchISO" = "true" ]; then
+	installerSetup
+	exit 0
+fi
+
+mainSetup
+exit 0
